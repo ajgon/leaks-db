c042ef3565c8cfbad4058e2190fb3834e85d0bff
Igor Rzegocki <igor@rzegocki.pl>
2017-12-01 15:25:43 +0100

    Initial commit

---
+++ api/docker-dev-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ ! -d "$APP_HOME/.ruby-advisory-db/.git" ]; then
+  rm -rf "$APP_HOME/.ruby-advisory-db"
+  git clone https://github.com/rubysec/ruby-advisory-db.git "$APP_HOME/.ruby-advisory-db"
+fi
+
+mkdir -p "$HOME/.local/share"
+ln -s "$APP_HOME/.ruby-advisory-db" "$HOME/.local/share/ruby-advisory-db"
+bundle-audit update
+cd "$APP_HOME" && git init && bundle exec overcommit --sign && overcommit --sign pre-commit
+
+exec "$@"
